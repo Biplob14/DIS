044533377468b697d16daa25afea6d5776f84d1a
Sadekur Rahaman <biploburrahaman@gmail.com>
2019-07-17 23:53:03 +0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,30 @@
+
+<footer class="page-footer footer"
+	style="position: fixed;
+		   left: 0;
+		   bottom: 0;
+		   width: 100%;">
+		   
+	<div class="container">
+		<div class="row">
+	    	<div class="col-lg-6" >
+	        	<p>Copyright &copy; My Website 2019</p>
+
+	    	</div>
+
+	    <div class="control-group">
+	        <div class="col-lg-3">
+	        	<a href="">Conact Us</a>
+	        </div>
+
+	        <div class="col-lg-3">
+	        	<a href="">about Us</a>
+			</div>
+	        		
+	  	</div>
+
+	</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-inverse navbar-light" style="background-color:  #38006f;">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php">Doctor Information System</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+
+
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Specialists<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Kidney & urology</a></li>
+            <li><a href="#">Heart</a></li>
+            <li><a href="#">Dermatology</a></li>
+          </ul>
+        </li>
+
+
+        <li><a href="#">Doctors</a></li>
+        <li><a href="#">Hospitals</a></li>
+
+      </ul>
+
+
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login.php" ><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
